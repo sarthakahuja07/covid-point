515885f65a708579c01018f0db14af970eed799a
sarthak <sarthak.ahuja0007@gmail.com>
2021-07-15 16:36:59 +0530

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAI0UF42U4MBNTBp-IHqXfnZx_szSWSSl0",
+    authDomain: "covid-point-79b5e.firebaseapp.com",
+    projectId: "covid-point-79b5e",
+    storageBucket: "covid-point-79b5e.appspot.com",
+    messagingSenderId: "580912881796",
+    appId: "1:580912881796:web:665a39f27595f0b525e2d1",
+    measurementId: "G-5V299298D9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
